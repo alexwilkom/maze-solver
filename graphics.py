4dbbd238ecb695b14d3c1306a4bdd93ef8fd7fda
Alex Wilkom <alex.wilkom@icloud.com>
2024-01-15 11:15:13 +0100

    Add missing dunderscore in root variable in Canvas' parent [line 7]

--- graphics.py
+++ graphics.py
@@ -4,7 +4,7 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__canvas = Canvas(self.root, bg="white", width=width, height=height)
+        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__is_running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from graphics import Window
+from graphics import Window, Line, Points
 
 def main():
     win = Window(800, 600)
+    line = Line(Points(50, 50), Points(400, 400))
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 main()
