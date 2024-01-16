e1f57e9e0df31b28fcef8b24055ff8903be66147
Alex Wilkom <alex.wilkom@icloud.com>
2024-01-16 11:55:48 +0100

    Add _reset_cells_visited method and a test for it

--- maze.py
+++ maze.py
@@ -23,12 +23,12 @@ class Maze:
         self._cell_size_y = cell_size_y
         self._win = win
         if seed:
-            self.seed = seed
-        else:
-            self.seed = random.seed(seed)
+            random.seed(seed)
 
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls(0, 0)
+        self._reset_cells_visited()
     
     def _create_cells(self):
         for _ in range(self._num_cols):
@@ -110,4 +110,9 @@ class Maze:
                 self._cells[i][j - 1].has_bottom_wall = False
 
             # recursively visit the next cell
-            self._break_walls_r(next_index[0], next_index[1])
+            self._break_walls(next_index[0], next_index[1])
+
+    def _reset_cells_visited(self):
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._cells[i][j].visited = False
